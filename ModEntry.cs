8933b47cea6c797aa9c3fb0ae48135cb5a3fa28e
bpavol <jorg.pavol@gmail.com>
2023-05-14 17:25:42 +0200

    Initial mod commit

---
+++ ModEntry.cs
@@ -0,0 +1,27 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using HarmonyLib;
+using HopperExtractor.Patches;
+
+namespace HopperExtractor
+{
+    internal sealed class ModEntry: Mod
+    {
+       /*********
+       ** Public methods
+       *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            var harmony = new Harmony(this.ModManifest.UniqueID);
+
+            ObjectPatches.Initialize(this.Monitor);
+            harmony.Patch(
+               original: AccessTools.Method(typeof(StardewValley.Object), nameof(StardewValley.Object.minutesElapsed)),
+               prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.After_MinutesElapsed))
+            );
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+﻿# Hopper extractor
+Hopper extracts items from machines.
+
+## Install
+1. Install the latest version of...
+   * [SMAPI](https://smapi.io);
+3. Extract mod files to `Mods/` folder.
+4. Run the game using SMAPI.
+
+## Use
+Hopper placed below machines will extract items and store them in internal storage when machine is ready for harvest.
+
+## Compatibility
+Tested on Stardew Valley 1.5.6, Windows, single-player and local multiplayer. Should work for other operating systems too.
+Compatibility with other mods is not tested, but should work unless mod patches same method `Object.minutesElapsed` as Hopper extractor mod.
